7947d1b8ca52a4e65107ed2e5d40e8421daa1fce
Abhijeet Singh Rathore <06abhi06@gmail.com>
2017-09-06 01:34:58 +0530

    Added FAB

--- app/src/main/java/com/example/abhijeet/todo/MainActivity.java
+++ app/src/main/java/com/example/abhijeet/todo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.abhijeet.todo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Click listener for the FAB ADD button
+        FloatingActionButton FAB = (FloatingActionButton) findViewById(R.id.addFAB);
+        FAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent editor_intent = new Intent(MainActivity.this,EditorActivity.class);
+                startActivity(editor_intent);
+            }
+        });
     }
 }
