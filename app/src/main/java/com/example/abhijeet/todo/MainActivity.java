2324ad64e49a2e5b4259c1a73a1c7ca2642c727b
Abhijeet Singh Rathore <06abhi06@gmail.com>
2017-09-09 01:29:08 +0530

    Implemented the TodoCursorloader.java

--- app/src/main/java/com/example/abhijeet/todo/MainActivity.java
+++ app/src/main/java/com/example/abhijeet/todo/MainActivity.java
@@ -1,22 +1,32 @@
 package com.example.abhijeet.todo;
 
+import android.app.LoaderManager;
 import android.content.ContentValues;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.ListView;
 
 import com.example.abhijeet.todo.Data.TodoContract;
 import com.example.abhijeet.todo.Data.TodoDbHelper;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    /**
+     * Identifier for the pet data loader
+     */
+    private static final int PET_LOADER = 0;
+
+    //Gloabal variable for the cursor adaptor as it will be used several times, hah just to put it in plain english.
+    private TodoCursorAdaptor mCursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +37,22 @@ public class MainActivity extends AppCompatActivity {
         FAB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent editor_intent = new Intent(MainActivity.this,EditorActivity.class);
+                Intent editor_intent = new Intent(MainActivity.this, EditorActivity.class);
                 startActivity(editor_intent);
             }
         });
+
+        // Find the ListView which will be populated with the to-do data
+        ListView listView = (ListView) findViewById(R.id.mainListView);
+
+
+        // Setup an Adapter to create a list item for each row of pet data in the Cursor.
+        // There is no pet data yet (until the loader finishes, cause its a Async task) so pass in null for the Cursor.
+        mCursorAdapter = new TodoCursorAdaptor(this, null);
+        listView.setAdapter(mCursorAdapter);
+
+        // Kick off the loader, as in start the loader. (Async task)
+        getLoaderManager().initLoader(PET_LOADER, null, this);
     }
 
     @Override
@@ -49,26 +71,40 @@ public class MainActivity extends AppCompatActivity {
                 values.put(TodoContract.TodoEntry.COLUMN_TASK, "Task 1 Time:");
                 values.put(TodoContract.TodoEntry.COLUMN_TIME, 1);
                 db.insert(TodoContract.TodoEntry.TABLE_NAME, null, values);
-                values.clear();
-                updatedata();
+                db.close();
                 return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
-    public void updatedata() {
 
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        // Define a projection that specifies the columns from the table we care about.
+        String[] projection = {
+                TodoContract.TodoEntry._ID,
+                TodoContract.TodoEntry.COLUMN_TASK,
+                TodoContract.TodoEntry.COLUMN_TIME};
 
-        Cursor cursor = getContentResolver().query(TodoContract.TodoEntry.CONTENT_URI, null, null, null, null);
-        TextView tv = (TextView) findViewById(R.id.Maintextview);
-        tv.setText("");
-        if (cursor == null)
-            Log.e("ABHIJEET", "NULL EXCEPTION");
-        while (cursor.moveToNext() != false) {
-            String task = cursor.getString(cursor.getColumnIndex(TodoContract.TodoEntry.COLUMN_TASK));
-            int time = cursor.getInt(cursor.getColumnIndex(TodoContract.TodoEntry.COLUMN_TIME));
-            tv.append(task + "" + time + "\n");
-        }
+        // This loader will execute the ContentProvider's query method on a background thread
+        return new CursorLoader(this,                 // Parent activity context
+                TodoContract.TodoEntry.CONTENT_URI,   // Provider content URI to query
+                projection,                           // Columns to include in the resulting Cursor
+                null,                                 // No selection clause
+                null,                                 // No selection arguments
+                null);                                // Default sort order
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor newDataCursor) {
+        //update the list View the new data after loading
+        mCursorAdapter.swapCursor(newDataCursor);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        //Callback called when the loader resets i.e. data needs to be deleted
+        mCursorAdapter.swapCursor(null);
 
     }
 }
---
+++ app/src/main/java/com/example/abhijeet/todo/TodoCursorAdaptor.java
@@ -0,0 +1,66 @@
+package com.example.abhijeet.todo;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import com.example.abhijeet.todo.Data.TodoContract;
+
+/**
+ * Created by Abhijeet on 9/9/2017.
+ */
+
+/**
+ * {@link TodoCursorAdaptor} is an adapter for a list or grid view
+ * that uses a {@link Cursor} of to-do data as its data source. This adapter knows
+ * how to create list items for each row of data in the {@link Cursor}.
+ */
+public class TodoCursorAdaptor extends CursorAdapter {
+
+    /**
+     * Constructs a new {@link TodoCursorAdaptor}.
+     *
+     * @param context The context
+     * @param c       The cursor from which to get the data.
+     */
+
+    public TodoCursorAdaptor(Context context, Cursor c) {
+        super(context, c, 0 /* FLAGS = 0*/);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+
+        // Inflate a list item view using the layout specified in list_item.xml
+        return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
+    }
+
+    /**
+     * This method binds the to-do data (in the current row pointed to by cursor) to the given
+     * list item layout. For example, the task for the current to-do can be set on the TextView
+     * in the list item layout.
+     *
+     * @param view    Existing view, returned earlier by newView() method
+     * @param context app context
+     * @param cursor  The cursor from which to get the data. The cursor is already moved to the
+     *                correct row.
+     */
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        //Get the relevant views where the info has to be shown
+        TextView tasktextview = (TextView) view.findViewById(R.id.task_show);
+        TextView timedatetextview = (TextView) view.findViewById(R.id.time_date_show);
+
+        //Read the relevant attributes from the cursor to display in the textviews
+        String task = cursor.getString(cursor.getColumnIndex(TodoContract.TodoEntry.COLUMN_TASK));
+        Integer time = cursor.getInt(cursor.getColumnIndex(TodoContract.TodoEntry.COLUMN_TIME));
+
+        //Update the Textviews
+        tasktextview.setText(task);
+        timedatetextview.setText(time.toString());
+    }
+}
